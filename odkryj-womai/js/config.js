0785cd968ccd13d0dc2e7d4b4c03f41c2d00340d
Piotr Tarasewicz <piotrktarasewicz@gmail.com>
2026-05-17 13:46:14 +0200

    Add Odkryj WOMAI request timeout configuration

--- odkryj-womai/js/config.js
+++ odkryj-womai/js/config.js
@@ -1,11 +1,15 @@
 window.WOMAI_CONFIG = {
   apiUrl: 'https://script.google.com/macros/s/AKfycbw4DT_QBC8em-TEXhP7BeAiiFZDrZqzSpLeOuy9HziSqlEVqwWiTbMql1qi0uSsuZ9v/exec',
+  requestTimeoutMs: 10000,
   guest: {
     sessionQuestionCountFallback: 4,
     preferredCategoryOrder: ['ciemność', 'światło', 'eksperymenty', 'womai'],
-    storageKey: 'womai_guest_progress_v2'
+    storageKey: 'womai_guest_progress_v2',
+    fallbackDataUrl: './data/fallback-questions.json',
+    requestTimeoutMs: 10000
   },
   admin: {
-    storageKey: 'womai_admin_v5_connection'
+    storageKey: 'womai_admin_v5_connection',
+    requestTimeoutMs: 15000
   }
 };
